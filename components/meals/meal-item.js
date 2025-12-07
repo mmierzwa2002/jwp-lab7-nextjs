ced30e57eac3da0e84d181ee6579e19f126360b8
Michał Mierzwa <123924536+mmierzwa2002@users.noreply.github.com>
2025-12-07 15:51:27 +0100

    Add meals components

--- components/meals/meal-item.js
+++ components/meals/meal-item.js
@@ -1,7 +1,6 @@
-import Link from 'next/link';
-import Image from 'next/image';
-
-import classes from './meal-item.module.css';
+import Link from "next/link";
+import Image from "next/image";
+import classes from "./meal-item.module.css";
 
 export default function MealItem({ title, slug, image, summary, creator }) {
   return (
