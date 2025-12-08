643915c8189fa3b0aee23f4297c1c2c23b92d830
Michał Mierzwa <>
2025-12-08 10:56:54 +0100

    Populate Meal Details page

--- app/meals/[someName]/page.js
+++
@@ -1,8 +0,0 @@
-export default function MealDetailsPage({ params }) {
-  return (
-    <main>
-      <h1>Meal Details</h1>
-      <p>You are looking at the meal: {params.someName}</p>
-    </main>
-  );
-}
