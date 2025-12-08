e8d4f36d18219975c180edc53da82f7fe371db34
Michał Mierzwa <>
2025-12-08 10:43:10 +0100

    Import Suspense

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
 
 async function Meals() {
   const meals = await getMeals();
